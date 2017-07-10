d6dc6ccbe1bc0fbdad69d38f93ba0500eff19567
Aleksandr Vasilev <alva0217@netcracker.com>
2017-07-10 16:56:19 +0400

    Initial commit

---
+++ src/main/java/alexander/lesson_2/Main.java
@@ -0,0 +1,12 @@
+package alexander.lesson_2;
+
+/**
+ * Classname
+ * Vesion Information
+ * Created by alva0217 on 10.07.2017.
+ */
+public class Main {
+    public static void main(String[] args){
+
+    }
+}
